1d97d6b45e2a9f51d5d6e6daa8679235259e3385
Shmidt Ivan <bahoc47@gmail.com>
2014-02-21 11:45:24 +0100

    done with widgets and shared cconsole.log

---
+++ www/js/app.js
@@ -0,0 +1,19 @@
+//
+//
+// Global object APP
+// You push all the APP-scoped info and methods here
+// For our purposes it will be: WebSockets data, serverSettings
+//
+//
+
+
+function App(){
+    var _this = this;
+}
+
+var app = new App();
+            app.servers = [
+                {serverName:"Server1", user:"root", password:"zckKqko12", server:"ws://37.139.4.54:57773/csp/cAdmin-Server/cAdmin.WebSocket.cls"},
+                {serverName:"Server2", user:"root", password:"zckKqko12", server:"ws://37.139.4.54:57773/csp/cAdmin-Server/cAdmin.WebSocket.cls"},
+                {serverName:"Server3", user:"root", password:"zckKqko12", server:"ws://37.139.4.54:57773/csp/cAdmin-Server/cAdmin.WebSocket.cls"}
+            ];
